5dd10cb9a6881c0dc100e8be45ca2c902294b845
Charles Luxinger <charlesluxinger@gmail.com>
2026-05-08 01:19:14 -0300

    Implement code quality tooling setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 	kotlin("plugin.jpa") version "2.3.10"
 	id("org.springframework.boot") version "3.5.14"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+	id("dev.detekt") version "2.0.0-alpha.2"
+	jacoco
 }
 
 group = "com.charlesluxinger.estaparking"
@@ -12,6 +15,16 @@ version = "0.0.1-SNAPSHOT"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	matching { it.name.startsWith("detekt") }.configureEach {
+		resolutionStrategy.eachDependency {
+			if (requested.group == "org.jetbrains.kotlin") {
+				useVersion("2.3.0")
+			}
+		}
 	}
 }
 
@@ -43,6 +56,43 @@ allOpen {
 	annotation("jakarta.persistence.Embeddable")
 }
 
+detekt {
+	toolVersion = "2.0.0-alpha.2"
+	buildUponDefaultConfig = true
+	config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+	baseline = file("$rootDir/config/detekt/baseline.xml")
+	ignoreFailures = false
+}
+
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		csv.required.set(false)
+		html.required.set(true)
+	}
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude("**/config/**", "**/health/**")
+                }
+            },
+        ),
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				minimum = "0.9".toBigDecimal()
+			}
+		}
+	}
 }
