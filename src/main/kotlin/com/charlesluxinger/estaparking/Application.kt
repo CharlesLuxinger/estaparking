9661acb31413e946c9a134e17d9f0cec30424612
Charles Luxinger <charlesluxinger@gmail.com>
2026-05-08 00:37:36 -0300

    project initializing

---
+++ src/main/kotlin/com/charlesluxinger/estaparking/Application.kt
@@ -0,0 +1,11 @@
+package com.charlesluxinger.estaparking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
